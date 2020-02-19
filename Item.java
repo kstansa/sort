a6dabf816d474669d5382cd9a1fe2ce0aa4dfb3e
kstansa <60517152+kstansa@users.noreply.github.com>
2020-02-19 12:59:14 -0600

    README, Bounded Item Constructor
    Added info to README
    Added constructor for Item that generates a random size within the range specified

--- Item.java
+++ Item.java
@@ -23,13 +23,31 @@ public class Item
     }
     
     /**
-     * Construcs Item with given id and random size
+     * Construcs Item with given id and random size between 0 and 1000
      * 
      * @param id item identifier
      */
     public Item(int id)
     {
         this(Math.random() * 1000, id);
+    }
+    
+    /**
+     * Construcs Item with given id and random size within specified range
+     * 
+     * @param id item identifier
+     * @param lowerBound lower bound of size range
+     * @param upperBound upper bound of size range
+     * @throws IllegalArgumentException if upperBound is not greater than lowerBound
+     */
+    public Item(int lowerBound, int upperBound, int id)
+    {
+        this(Math.random() * (upperBound - lowerBound) - lowerBound, id);
+        //throw exception if bounds are invalid
+        if(upperBound < lowerBound)
+        {
+            throw new IllegalArgumentException("upperBound must be greater than lowerBound");
+        }
     }
     
     public double getSize()
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sort
  Demonstrates various sorting methods.
+
+Uses abstract methods such that each sorting method is a different object type.
+
+Coded in Java 1.6 in accordance with the College Board AP Computer Science curriculum.
